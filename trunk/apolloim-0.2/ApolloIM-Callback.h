4b20fc58a24db44afd27d02e2d87867b6738bd4f
Arminius2 <Arminius2@2a44d256-ae37-0410-86ee-955858c3cfb1>
2007-09-08 17:01:40 +0000

    0.2 baby

---
+++ trunk/apolloim-0.2/ApolloIM-Callback.h
@@ -0,0 +1,41 @@
+/*
+ ApolloTOC.m: Objective-C firetalk interface.
+ By Alex C. Schaefer
+ Modification of BoomBot's objc/firetalk wrapper
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+void ft_callback_doinit				(void *c, void *cs, char *nickname);
+void ft_callback_im_user_nickchanged(void *c, void *cs, const char * const nickname);
+void ft_callback_listbuddy			(void *c, void *cs, const char * const nickname, const char * const group, char online, char away, const long idle);
+
+void ft_callback_buddytyping		(void *c, void *cs, const char * const who, const int typing);
+void ft_callback_buddyidle			(void *c, void *cs, const char * const who, const long idle);
+void ft_callback_buddystatus		(void *c, void *cs, const char * const who, const char *message);
+
+void ft_callback_buddyonline		(void *c, void *cs, const char * const who);
+void ft_callback_buddyoffline		(void *c, void *cs, const char * const who);
+void ft_callback_buddyunaway		(void *c, void *cs, const char * const who);
+void ft_callback_buddyaway			(void *c, void *cs, const char * const who);
+void ft_callback_getinfo			(void *c, void *cs, const char * const who, const char * const info, const int warning, const int idle, const int flags);
+void ft_callback_getaction			(void *c, void *cs, const char * const room, const char * const from, const int automessage, const char * message);
+void ft_callback_error				(void *c, void *cs, const int error, const char * const roomoruser, const char * const description);
+void ft_callback_connectfailed		(void *c, void *cs, int error, char *reason);
+void ft_callback_getmessage			(void *c, void *cs, const char * const who, const int automessage, const char * const message);
+void ft_callback_disconnect			(void *c, void *cs, const int error);
+void ft_callback_needpass			(void *c, void *cs, char *p, const int size);
+void ft_callback_storepass			(char* newpass);
+
---
+++ trunk/apolloim-0.2/main.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "ApolloIMApp.h"
+
+int main(int argc, char **argv) 
+{
+	[[NSAutoreleasePool alloc] init];
+	return UIApplicationMain(argc, argv, [ApolloIMApp class]);
+}
