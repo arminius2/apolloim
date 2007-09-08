ff6b80fa9ef07c46bc6a71d31e1686881dcc7f0d
Arminius2 <Arminius2@2a44d256-ae37-0410-86ee-955858c3cfb1>
2007-09-08 16:12:55 +0000

    clean branches

--- branches/nightwatch/apolloim/main.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "ApolloIMApp.h"
-
-int main(int argc, char **argv) 
-{
-    [[NSAutoreleasePool alloc] init];
-    return UIApplicationMain(argc, argv, [ApolloIMApp class]);
-}
