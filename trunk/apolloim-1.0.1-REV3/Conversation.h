869b7e4081dd46abcf4c34af43cbbfc82709f623
alex.mellmo <alex.mellmo@2a44d256-ae37-0410-86ee-955858c3cfb1>
2008-02-01 05:27:22 +0000

    header update

--- trunk/apolloim-1.0.1-REV3/Conversation.h
+++ trunk/apolloim-1.0.1-REV3/Conversation.h
@@ -1,6 +1,6 @@
 /*
- ApolloTOC.m: Objective-C firetalk interface.
- By Alex C. Schaefer
+ Apollo: Libpurple based Objective-C IM Client
+ By Alex C. Schaefer & Adam Bellmore
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -15,6 +15,13 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ Portions of this code are referenced from "Libpurple", courtesy of www.pidgin.im
+ as well as AdiumX (www.adiumx.com).  This code is full GPLv2, and a GPLv2.txt 
+ is contained in the source and program root for you to read.  If not, please
+ refer to the above address to obtain your own copy.
+ 
+ Any questions or comments should be posted at http://apolloim.googlecode.com
 */
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
--- trunk/apolloim-1.0.1-REV3/SendBox.h
+++ trunk/apolloim-1.0.1-REV3/SendBox.h
@@ -1,10 +1,28 @@
-//
-//  SendBox.h
-//  ApolloIM
-//
-//  Created by Alex C. Schaefer on 8/29/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
-//
+/*
+ Apollo: Libpurple based Objective-C IM Client
+ By Alex C. Schaefer & Adam Bellmore
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ Portions of this code are referenced from "Libpurple", courtesy of www.pidgin.im
+ as well as AdiumX (www.adiumx.com).  This code is full GPLv2, and a GPLv2.txt 
+ is contained in the source and program root for you to read.  If not, please
+ refer to the above address to obtain your own copy.
+ 
+ Any questions or comments should be posted at http://apolloim.googlecode.com
+*/
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <UIKit/CDStructures.h>
