978d218f0b59ed4d5fbb367f1505b5c170221841
alex.mellmo <alex.mellmo@2a44d256-ae37-0410-86ee-955858c3cfb1>
2008-02-01 05:27:22 +0000

    header update

--- apolloim-1.0.1-REV3/AdiumPurpleDnsRequest.h
+++ apolloim-1.0.1-REV3/AdiumPurpleDnsRequest.h
@@ -1,18 +1,28 @@
-/* 
- * Adium is the legal property of its developers, whose names are listed in the copyright file included
- * with this source distribution.
- * 
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this program; if not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+/*
+ Apollo: Libpurple based Objective-C IM Client
+ By Alex C. Schaefer & Adam Bellmore
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ 
+ Portions of this code are referenced from "Libpurple", courtesy of www.pidgin.im
+ as well as AdiumX (www.adiumx.com).  This code is full GPLv2, and a GPLv2.txt 
+ is contained in the source and program root for you to read.  If not, please
+ refer to the above address to obtain your own copy.
+ 
+ Any questions or comments should be posted at http://apolloim.googlecode.com
+*/
  
  //
 //  adiumPurpleDnsRequest.h
@@ -20,7 +30,7 @@
 //
 //  Created by Graham Booker on 2/24/07.
 //
-//	From the Adium project.
+//	From the AdiumX project. -- www.adiumx.com
 //Purple
 #include <libpurple/internal.h>
 #include <libpurple/account.h>
